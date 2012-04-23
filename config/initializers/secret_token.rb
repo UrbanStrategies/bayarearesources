04c29e745a0b4e0bf44969cbecea129d75bada92
Joel Mahoney <joelmahoney@gmail.com>
2012-04-22 17:56:53 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bayarearesources::Application.config.secret_token = '0068acb49d0b7639938c7f4561a98f8eaecb099a1c858d2af019442d8bcefd9855613d34b8d63f7a16af89b7568aa3fb1a4907d70072bd65b912670015df7686'
