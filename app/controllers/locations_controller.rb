405508aa1f989c400fc44d36fd26eaf2ae694a11
Joel Mahoney <joelmahoney@gmail.com>
2012-04-24 23:11:50 -0700

    Clean up links and routes

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,5 +1,4 @@
 class LocationsController < ApplicationController
-  before_filter :load_variables
   
   def index
     @locations = Location.all
@@ -15,12 +14,5 @@ class LocationsController < ApplicationController
     respond_to do |format|
       format.html # show.html.erb
     end
-  end
-  
-  private
-  def load_variables
-    @organization = Organization.find(params[:organization_id])
-    @organizations = Organization.all(:order => :name)
-    @services = Service.all(:order => :name)
   end
 end
