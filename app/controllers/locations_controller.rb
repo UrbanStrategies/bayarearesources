87fa348c7d34c15126fe3b249ddd7b622036b5d0
Joel Mahoney <joelmahoney@gmail.com>
2012-04-25 01:19:38 -0700

    Work on locations search

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,8 +1,14 @@
 class LocationsController < ApplicationController
   
   def index
-    @locations = Location.all
-
+    if params[:address].present?
+      @locations = Location.near(params[:address], 100)
+      @address_placehoder = params[:address]
+    else
+      @locations = Location.all
+      @address_placehoder = 'Address City, State'
+    end
+    
     respond_to do |format|
       format.html # index.html.erb
     end
