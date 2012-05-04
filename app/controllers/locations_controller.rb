a2724bb4ec1f4772feae6d11cdef8a2d408e5ced
Joel Mahoney <joelmahoney@gmail.com>
2012-05-03 23:27:03 -0700

    Pass address params through js actions so things work properly

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -5,20 +5,8 @@ class LocationsController < ApplicationController
     @services = Service.all(:order=>:name)
     session[:category_ids] ||= Category.all.collect {|x| x.id}
     session[:service_ids] ||= []
+    params[:miles] ||= 10
     
-    # Address Locations
-    
-    if params[:address].present?
-      @address_placehoder = params[:address]
-      session[:address] = params[:address]
-      address_locations = Location.near(session[:address], 100)
-      address_locations.present? ? location_ids = address_locations.collect {|x| x.id} : location_ids = []
-    else
-      @address_placehoder = 'Your Address' 
-      session[:address] = nil
-      location_ids = Location.all.collect {|x| x.id}
-    end
-      
     # Category Locations
     
     if params[:add_category].present?
@@ -31,9 +19,18 @@ class LocationsController < ApplicationController
     session[:category_ids].each do |cat_id|
       category_location_ids << Category.find(cat_id).locations.collect {|x| x.id}
     end
-    location_ids = location_ids & category_location_ids.flatten
+    category_location_ids.try(:flatten!).try(:uniq!)
     
-    @locations = Location.find(location_ids)
+    # Address Locations
+    
+    if params[:address].present?
+      @address_placehoder = params[:address]
+      @locations = Location.where('id IN (?)', category_location_ids).near(params[:address], params[:miles])
+    else
+      @address_placehoder = 'Your Address' 
+      @locations = Location.find(category_location_ids)
+    end
+        
     @json = @locations.to_gmaps4rails
     
     respond_to do |format|
