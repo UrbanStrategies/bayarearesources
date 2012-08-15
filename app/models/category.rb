a3ac563e2dec2fc9e81d65ea8d9f8559c568f358
Joel Mahoney <joelmahoney@gmail.com>
2012-08-15 05:46:20 -0700

    Make sure associations are uniq

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ActiveRecord::Base
-  has_many :services, :order => :name, :dependent => :destroy
-  has_many :locations, :through => :services
+  has_many :services, :order => :name, :dependent => :destroy, :uniq => true
+  has_many :locations, :through => :services, :uniq => true
   
   attr_accessible :description, :name
 end
