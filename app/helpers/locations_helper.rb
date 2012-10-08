e69bad66a3e91913b75ef9f107773e63587a3bbb
Joel Mahoney <joelmahoney@gmail.com>
2012-10-08 15:49:29 -0700

    Configure language checkboxes

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -7,4 +7,19 @@ module LocationsHelper
       image_tag('cancel.png')
     end
   end
+  
+  def class_tags(location)
+    tags = []
+    location.categories.each do |category|
+      if category.present?
+        tags << 'category_' + category.id.to_s
+      end
+    end
+    location.languages.each do |language|
+      if language.present?
+        tags << 'language_' + language.id.to_s + ' '
+      end
+    end
+    return tags.join(' ')
+  end
 end
