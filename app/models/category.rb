cd84f2710c300bf198c442047e281f5c512a04f5
Joel Mahoney <joelmahoney@gmail.com>
2012-12-19 12:38:53 -0800

    Filter results counts

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,8 @@ class Category < ActiveRecord::Base
   has_many :locations, :through => :services, :uniq => true
   
   attr_accessible :description, :name
+  
+  def results_in_set(locations_set)
+    (self.locations & locations_set).try(:size)
+  end
 end
