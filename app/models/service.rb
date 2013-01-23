271719028bdccdb19ebcd8255c1abce42d31c29c
Joel Mahoney <joelmahoney@gmail.com>
2013-01-23 15:32:24 -0800

    Add category_id to accessible attributes list

--- app/models/service.rb
+++ app/models/service.rb
@@ -2,7 +2,7 @@ class Service < ActiveRecord::Base
   belongs_to :category
   has_and_belongs_to_many :locations, :uniq => true
   
-  attr_accessible :description, :name
+  attr_accessible :category_id, :name, :description
   
   def results_in_set(locations_set)
     (self.locations & locations_set).try(:size)
