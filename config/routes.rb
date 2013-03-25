5a38f897c2d1389ff7b6a0b10d921f382a98efca
Joel Mahoney <joelmahoney@gmail.com>
2013-03-24 22:05:20 -0700

    Remove unused controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Bayarearesources::Application.routes.draw do
   root :to => 'locations#index'   
   resources :locations, :only => [:index, :show]
-  resources :services
   devise_for :users
   
   
